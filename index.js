d98237b31823189d428cd54d66d8c2dc038da88c
Simon Parker <lastpolar@gmail.com>
2025-05-26 14:50:42 +1000

    fixing CORS crashing from unknown origins

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ app.use(
   cors({
     origin: function (origin, callback) {
       // allow requests with no origin (like mobile apps, curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
       }
+      return callback(null, false); // quietly reject
     },
     methods: ["GET", "POST", "DELETE"],
   })
